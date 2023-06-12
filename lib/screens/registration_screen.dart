7c9d7f1d7d8ed4bc7e1ad8c57f58a09ade390b5f
Macdeo <akinbiyi.adeola@gmail.com>
2023-06-12 17:20:39 +0200

    modified:   .flutter-plugins-dependencies 	modified:   android/app/src/main/AndroidManifest.xml 	modified:   android/app/src/main/java/co/appbrewery/flash_chat/MainActivity.java 	modified:   ios/Flutter/Debug.xcconfig 	new file:   ios/Flutter/Flutter.podspec 	modified:   ios/Flutter/Release.xcconfig 	modified:   ios/Flutter/flutter_export_environment.sh 	new file:   ios/Podfile 	new file:   ios/Podfile.lock 	modified:   ios/Runner.xcodeproj/project.pbxproj 	modified:   ios/Runner.xcworkspace/contents.xcworkspacedata 	modified:   lib/main.dart 	modified:   lib/screens/chat_screen.dart 	modified:   lib/screens/login_screen.dart 	modified:   lib/screens/registration_screen.dart

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration_screen';
@@ -9,6 +11,9 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,7 +35,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
                 onChanged: (value) {
+                  email = value;
                   //Do something with the user input.
                 },
                 decoration: kTextFieldDecoration.copyWith(
@@ -39,8 +47,11 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 8.0,
             ),
             TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
                 onChanged: (value) {
                   //Do something with the user input.
+                  password = value;
                 },
                 decoration: kTextFieldDecoration.copyWith(
                     hintText: 'Enter your password')),
@@ -50,7 +61,20 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             RoundedButtons(
                 color: Colors.blueAccent,
                 buttonTitle: 'Register',
-                onPressed: () {}),
+                onPressed: () async {
+                  print(email);
+                  print(password);
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+
+                    if (newUser != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                  } catch (e) {
+                    print(e);
+                  }
+                }),
           ],
         ),
       ),
